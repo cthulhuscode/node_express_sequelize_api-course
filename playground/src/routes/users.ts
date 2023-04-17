a07f6155075fdae894191ce4aa8286c74f9b3c78
Enrique Ruvalcaba <jordison352@gmail.com>
2023-04-17 17:24:15 -0600

    Added users route validation

--- playground/src/routes/users.ts
+++ playground/src/routes/users.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/users";
+import { validatorHandler } from "../middlewares/validatorHandler";
+import { getUserSchema } from "../schemas/users";
 
 export const router = Router();
 
+router.get("/:id", validatorHandler(getUserSchema, "params"), getUser);
 router.get("/", getUsers);
-router.get("/:id", getUser);
--- playground/src/schemas/users.ts
+++ playground/src/schemas/users.ts
@@ -1,4 +1,28 @@
 import Joi from "joi";
 
-const id = Joi.number().positive().message("Invalid id.");
-const email = Joi.string().email().message("Invalid email.");
+const id = Joi.number()
+  .positive()
+  .required()
+  .messages({ "any.only": "Invalid id." });
+const email = Joi.string().email().trim().message("Invalid email.");
+const password = Joi.string().min(4).trim();
+const createdAt = Joi.date().optional();
+
+export const getUserSchema = Joi.object({
+  id,
+});
+
+export const deleteUserSchema = Joi.object({
+  id,
+});
+
+export const createUserSchema = Joi.object({
+  email: email.required(),
+  password: password.required(),
+  createdAt: createdAt.optional(),
+});
+
+export const updateUserSchema = Joi.object({
+  email: email,
+  password: password,
+});
