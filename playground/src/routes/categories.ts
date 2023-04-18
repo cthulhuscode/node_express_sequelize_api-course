a1866855ab37cb2434fff40fd30c6941bb10b66b
Enrique Ruvalcaba <jordison352@gmail.com>
2023-04-18 14:44:01 -0600

    validated routes

--- playground/src/routes/categories.ts
+++ playground/src/routes/categories.ts
@@ -7,12 +7,27 @@ import {
   deleteCategory,
   getCategory,
 } from "../controllers/categories";
+import {
+  createCategorySchema,
+  getCategorySchema,
+  updateCategorySchema,
+} from "../schemas/categories";
+import { validatorHandler } from "../middlewares/validatorHandler";
 
 export const router = Router();
 
+router.get("/:id", validatorHandler(getCategorySchema, "params"), getCategory);
+router.put("/:id", validatorHandler(updateCategorySchema, "body"), putCategory);
+router.patch(
+  "/:id",
+  validatorHandler(updateCategorySchema, "body"),
+  patchCategory
+);
+router.delete(
+  "/:id",
+  validatorHandler(getCategorySchema, "params"),
+  deleteCategory
+);
+
+router.post("/", validatorHandler(createCategorySchema, "body"), addCategory);
 router.get("/", getCategories);
-router.get("/:id", getCategory);
-router.post("/", addCategory);
-router.put("/:id", putCategory);
-router.patch("/:id", patchCategory);
-router.delete("/:id", deleteCategory);
