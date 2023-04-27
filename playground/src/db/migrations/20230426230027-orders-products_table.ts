f88eb98c8b57654c2e84ba09a8c122c12eb23d0a
Enrique Ruvalcaba <jordison352@gmail.com>
2023-04-26 18:13:35 -0600

    add orders-product table migration

---
+++ playground/src/db/migrations/20230426230027-orders-products_table.ts
@@ -0,0 +1,18 @@
+"use strict";
+"use strict";
+
+import {
+  OrderProductSchema,
+  ORDERS_PRODUCTS_TABLE,
+} from "../models/order-product";
+
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  async up(queryInterface: any, Sequelize: any) {
+    await queryInterface.createTable(ORDERS_PRODUCTS_TABLE, OrderProductSchema);
+  },
+
+  async down(queryInterface: any, Sequelize: any) {
+    await queryInterface.dropTable(ORDERS_PRODUCTS_TABLE);
+  },
+};
